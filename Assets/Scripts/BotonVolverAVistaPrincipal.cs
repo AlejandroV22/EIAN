6edcc903112ab3ffd5b7e6d89e65515e9c4fcb22
AlejandroV <alejandrovelandiagelvez@gmail.com>
2025-08-23 18:34:00 -0500

    Mejoras visuales significativas

--- Assets/Scripts/BotonVolverAVistaPrincipal.cs
+++ Assets/Scripts/BotonVolverAVistaPrincipal.cs
@@ -1,10 +1,37 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using TMPro; // Importante para usar TextMeshProUGUI
 
-public class BotonVolverAVistaPrincipal : MonoBehaviour
+public class BotonVolverAVistaPrincipal : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("Nombre de la escena a cargar")]
     public string nombreEscena;
+
+    private TextMeshProUGUI texto;
+    private int tamañoOriginal;
+
+    void Start()
+    {
+        
+        texto = GetComponentInChildren<TextMeshProUGUI>();
+        if (texto != null)
+        {
+            tamañoOriginal = Mathf.RoundToInt(texto.fontSize);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (texto != null)
+            texto.fontSize = tamañoOriginal + 7; 
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (texto != null)
+            texto.fontSize = tamañoOriginal;
+    }
 
     public void Volver()
     {
