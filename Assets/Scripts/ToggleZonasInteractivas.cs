8ee3d20fdfa65a9c1869ad50353734924a3305bd
AlejandroV22 <alejandrovelandiagelvez@gmail.com>
2025-09-22 15:33:38 -0500

    Mejora de zonas a franjas interactivas

--- Assets/Scripts/ToggleZonasInteractivas.cs
+++ Assets/Scripts/ToggleZonasInteractivas.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class ToggleZonasInteractivas : MonoBehaviour
 {
@@ -7,8 +8,23 @@ public class ToggleZonasInteractivas : MonoBehaviour
     public Toggle toggle;
     public GameObject zonasInteractivas;
 
+    // Guardar alpha original de cada imagen
+    private Dictionary<Image, float> alphaOriginal = new Dictionary<Image, float>();
+
     void Start()
     {
+        if (zonasInteractivas != null)
+        {
+            foreach (Transform child in zonasInteractivas.transform)
+            {
+                Image image = child.GetComponent<Image>();
+                if (image != null && !alphaOriginal.ContainsKey(image))
+                {
+                    alphaOriginal[image] = image.color.a;
+                }
+            }
+        }
+
         if (toggle != null && zonasInteractivas != null)
         {
             toggle.onValueChanged.AddListener(ToggleZonas);
@@ -23,12 +39,15 @@ public class ToggleZonasInteractivas : MonoBehaviour
             foreach (Transform child in zonasInteractivas.transform)
             {
                 Image image = child.GetComponent<Image>();
-                if (image != null)
+                if (image != null && alphaOriginal.ContainsKey(image))
                 {
                     Color color = image.color;
-                    color.a = estado ? 1f : 0f;
+                    // Restaurar su alpha original o ponerlo en 0
+                    color.a = estado ? alphaOriginal[image] : 0f;
                     image.color = color;
-                    image.raycastTarget = true; // Asegurar que siga detectando clics
+
+                        // Activar o desactivar la interactividad
+                    image.raycastTarget = true;
                 }
             }
         }
--- Assets/Scripts/ZonaInteractiva2.cs
+++ Assets/Scripts/ZonaInteractiva2.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ZonaInteractiva2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class ZonaInteractiva2 : MonoBehaviour, IPointerClickHandler
 {
-    public Color hoverColor = new Color(1f, 1f, 1f, 0.3f);
-    private Image image;
-    private Color originalColor;
+   
 
     public string infoTexto;
     public InfoPanel panel;
@@ -15,27 +13,9 @@ public class ZonaInteractiva2 : MonoBehaviour, IPointerEnterHandler, IPointerExi
     public string zonaID;
     public string temaID;
 
-    void Start()
-    {
-        image = GetComponent<Image>();
-        originalColor = image.color;
-    }
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        if (image.color.a != 0)
-        {
-            image.color = hoverColor;
-        }
-    }
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        if (image.color.a != 0)
-        {
-            image.color = originalColor;
-        }
-    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         panel.MostrarInformacion(infoTexto);
