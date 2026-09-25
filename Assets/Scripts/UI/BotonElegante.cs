e234c18e54a696098c3f87096e983e5e385afbcf
AlejandroV <alejandrovelandiagelvez@gmail.com>
2026-09-25 11:13:42 -0500

    menu principal mejorado, añadida escena de press start

--- Assets/Scripts/UI/BotonElegante.cs
+++ Assets/Scripts/UI/BotonElegante.cs
@@ -1,28 +1,35 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
+using TMPro;
 
 public class BotonElegante : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-
     private Vector3 escalaOriginal;
+    private Color colorOriginal;
+    private TMP_Text textoBoton;
+
+    public float escalador = 1.15f;
+    public GameObject flecha;
 
     void Start()
     {
-
+        flecha.SetActive(false);
         escalaOriginal = transform.localScale;
+        textoBoton = GetComponentInChildren<TMP_Text>();
+        colorOriginal = textoBoton.color;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-
-        transform.localScale = escalaOriginal * 1.37f;
+        transform.localScale = escalaOriginal * escalador;
+        flecha.SetActive(true);
+        textoBoton.color = new Color(0f, 0f, 145f / 255f);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-
-        
         transform.localScale = escalaOriginal;
+        flecha.SetActive(false);
+        textoBoton.color = colorOriginal;
     }
 }
